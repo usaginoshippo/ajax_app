33c06b2869a1d8a46a3c0de57be93e2b5959b01c
usaginoshippo <86910445+usaginoshippo@users.noreply.github.com>
2021-10-17 16:52:00 +0900

    JSでレスポンスを処理する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,3 +1,17 @@
+const htmlBuild = (XHR)=>{
+  const item = XHR.response.post;
+  const html = `
+    <div class="post">
+      <div class="post-date">
+      投稿日時:${item.created_at}
+      </div>
+      <div class="post-content">
+      ${item.content}
+      </div>
+    </div>`;
+    return html;
+};
+
 function post(){
   const submit = document.getElementById("submit");
   submit.addEventListener("click",(e) =>{
@@ -5,9 +19,19 @@ function post(){
     const form = document.getElementById("form");
     const formData = new FormData(form);
     const XHR = new XMLHttpRequest();
-    XHR.open("POST","/posts", true);
-    XHR.responseType="json";
-    XHR.send(formData);
-  });
+      XHR.open("POST","/posts", true);
+      XHR.responseType="json";
+      XHR.send(formData);
+      XHR.onload=()=>{
+        if (XHR.status != 200){
+          alert(`Error${XHR.status}: ${XHR.statusText}`)
+          return null;
+        };
+        const list = document.getElementById("list");
+        const formText = document.getElementById("content");
+        list.insertAdjacentHTML("afterend",htmlBuild(XHR));
+        formText.value="";
+      };
+    });
   };
 window.addEventListener('load',post);
